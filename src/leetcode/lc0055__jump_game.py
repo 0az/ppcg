7bac1d3399e33cd85514cd1e7697ea266fe72a98
Andrew Zhou <0az@afzhou.com>
2020-08-26 16:53:53 -0500

    feat(lc0055): Complete Jump Game I

---
+++ src/leetcode/lc0055__jump_game.py
@@ -0,0 +1,36 @@
+"""
+Solution for LC0055: Jump Game (Medium).
+
+Given an array of non-negative integers, you are initially positioned at the
+first index of the array. Each element in the array represents your maximum
+jump length at that position. Determine if you are able to reach the last
+index.
+"""
+
+from typing import List
+
+from ppcg import case
+
+_LC_ENTRYPOINT_ = 'canJump'
+_LC_EXPORT_ = 'jump_game_reverse'
+_LC_CASES_ = [
+    case(True, [2, 3, 1, 1, 4]),
+    case(False, [3, 2, 1, 0, 4]),
+]
+
+
+
+def jump_game_reverse(board: List[int]) -> bool:
+    target = len(board) - 1
+
+    if target <= 0:
+        return True
+
+    i = target - 1
+    while i >= 0:
+        elem = board[i]
+        if i + elem >= target:
+            target = i
+        i -= 1
+
+    return not target
