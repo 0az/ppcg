7bca89aac8a1e3a0078e2ae00a98459cf86c205d
Andrew Zhou <0az@afzhou.com>
2020-09-05 16:45:01 -0500

    feat(lc0067): Complete add binary

---
+++ src/leetcode/lc0067__add_binary.py
@@ -0,0 +1,65 @@
+"""
+Add two numbers, encoded as binary strings.
+"""
+
+from hypothesis import assume, given
+from hypothesis.strategies import from_regex
+
+_LC_EXPORT_ = 'add_binary'
+_LC_ENTRYPOINT_ = 'addBinary'
+
+
+def add_binary(left: str, right: str):
+    diff = len(left) - len(right)
+
+    if diff < 0:
+        left, right = right, left
+        diff = -diff
+
+    result = []
+    carry = False
+
+    for i, (l, r) in enumerate(zip(reversed(left), reversed(right))):
+        if l == r:
+            result.append(carry)
+            carry = l == '1'
+        elif l == '1' or r == '1':
+            if carry:
+                result.append(False)
+                carry = True
+            else:
+                result.append(True)
+                carry = False
+        else:
+            result.append(carry)
+            carry = False
+
+    if diff:
+        for c in left[diff - 1 :: -1]:  # range(diff - 1, -1, -1):
+            if carry and c == '1':
+                result.append(False)
+                carry = True
+            elif carry or c == '1':
+                result.append(True)
+                carry = False
+            else:
+                result.append(False)
+                carry = False
+
+    if carry:
+        result.append(True)
+    return ''.join(map(str, map(int, reversed(result))))
+
+
+def reference_add_binary(left: str, right: str) -> str:
+    return bin(int(left, 2) + int(right, 2))[2:]
+
+
+@given(
+    from_regex(f'1[01]*|0', fullmatch=True),
+    from_regex(f'1[01]*|0', fullmatch=True),
+)
+def test_add_binary(left: str, right: str):
+    assert (left, right) and add_binary(left, right) == reference_add_binary(
+        left, right
+    )
