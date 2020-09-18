4123190b7b87bbc31e482ebff3f568c7bb477e83
Andrew Zhou <0az@afzhou.com>
2020-09-18 13:58:47 -0500

    feat(ppcg): Implement basic template generation

---
+++ src/ppcg/_template.py
@@ -0,0 +1,17 @@
+import hypothesis.strategies as st
+from hypothesis import given
+
+_LC_ENTRYPOINT_ = ''
+_LC_EXPORT_ = ''
+_LC_DESCRIPTION_ = '''
+'''
+
+
+# TODO
+
+
+@given(
+    # TODO
+)
+def test_() -> None:
+    pass
--- src/ppcg/cli.py
+++ src/ppcg/cli.py
@@ -1,10 +1,13 @@
+import os
 import sys
 from pathlib import Path
-from typing import Tuple
+from tempfile import mkstemp
+from typing import Any, Dict, Tuple
 
 import pyperclip
 
 from ._core import LeetcodeSolution
+from .utils import format_contents
 
 HELP = '''
 Usage:
@@ -13,22 +16,24 @@ Usage:
     {program} help
 '''
 
+TEMPLATE_PATH = Path(__file__).parent.resolve() / '_template.py'
+
 
 def print_help() -> int:
     print(HELP.format(program='ppcg', file=sys.stderr))
     return 0
 
 
-def _command_print(*argv) -> Tuple[str, int]:
+def _command_print(*argv) -> str:
     if len(argv) != 1:
         print_help()
-        return '', 1
+        raise SystemExit(1)
 
     solution = Path(argv[0])
 
     if not solution.exists():
         print(f'Solution {solution} not found', file=sys.stderr)
-        return '', 1
+        raise SystemExit(1)
 
     result = LeetcodeSolution.parse(solution).serialize(
         imports=False,
@@ -38,28 +43,57 @@ def _command_print(*argv) -> Tuple[str, int]:
         #
     )
 
-    return result, 0
+    return result
 
 
 def command_print(*argv) -> int:
-    result, err = _command_print(*argv)
-
-    if err:
-        return err
+    result = _command_print(*argv)
 
     print(result, end='')
     return 0
 
 
 def command_copy(*argv: str) -> int:
-    result, err = _command_print(*argv)
-
-    if err:
-        return err
+    result = _command_print(*argv)
 
     pyperclip.copy(result)
     lines = sum(1 for c in result if c == '\n')
     print(f'Copied {lines} lines!', file=sys.stderr)
+    return 0
+
+
+def _command_generate(ctx: Dict['str', Any] = None) -> str:
+    template = TEMPLATE_PATH.read_text()
+    if ctx:
+        return template.format_map(ctx)
+    return template
+
+
+def command_generate(*argv: str) -> int:
+    if len(argv) > 1:
+        return 1
+
+    text = format_contents(_command_generate())
+    if not len(argv):
+        print(text, end='')
+        return 0
+
+    out = Path(argv[0]).resolve()
+
+    fd, name = mkstemp(prefix='ppcg', text=True)
+    try:
+        with os.fdopen(fd, 'w+') as f:
+            print(text, end='', file=f)
+        os.replace(name, out)
+    finally:
+        try:
+            os.close(fd)
+        except OSError as e:
+            pass
+
+        if os.path.exists(name):
+            os.unlink(name)
+
     return 0
 
 
@@ -81,6 +115,9 @@ def main():
 
     if cmd == 'copy':
         return command_copy(*rest)
+
+    if cmd == 'generate':
+        return command_generate(*rest)
 
     print_help()
     return 0
